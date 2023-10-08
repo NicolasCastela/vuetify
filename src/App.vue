ba3c09f87857debdbc44d97f0ba4d39035deb740
NicolasCastela <nickcastela2022@hotmail.com>
2023-10-08 17:19:21 -0300

    att-12

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,87 @@
 <template>
-  <v-app theme="dark">
+  <v-app theme="">
+    <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-subheader>Menu</v-list-subheader>
+        <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+        <v-list-item prepend-icon="mdi-account">Usuário</v-list-item>
+        <v-list-group value="usClientes">
+          <template #activator="{ props }">
+          
+            <v-list-item
+              v-bind="props"
+              prepend-icon="mdi-account-circle"
+              title="Clientes">
+            </v-list-item>
+          </template> 
+          <v-list-item prepend-icon="mdi-currency-usd">Faturamento</v-list-item>
+          <v-list-item prepend-icon="mdi-chart-line">Relatório</v-list-item>
+        </v-list-group>
+        
+      </v-list>
+    </v-navigation-drawer>
+
+
+
     <v-app-bar flat class="border-b" title="">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="isDrawerOpen  = !isDrawerOpen "></v-app-bar-nav-icon>
       <v-app-bar-tittle>Meu titulo</v-app-bar-tittle>
+      <!-- NOTIFICAÇÃO -->
       <template #append>
-        <v-btn icon class="mr-2">
-         <v-badge :content="9" color="info"> 
-          <v-icon icon="mdi-bell-outline"></v-icon>
-        </v-badge>
-        </v-btn>
-       
-        <v-avatar >
+        <v-menu>
+          <template #activator="{props}">
+            <v-btn v-bind:="props" icon class="mr-2">
+              <v-badge v-bind:="props" :content="9" color="info"> 
+              
+                  <v-icon icon="mdi-bell-outline">
+                  </v-icon>        
+              
+                </v-badge>
+          </v-btn>
+          </template>
+          <v-card  min-width="300px" min-height="200px ">
+    <v-list lines="false" density="compact" >
+      <!-- 1   -->
+      <v-list-item flat class="border-b" prepend-icon="mdi-alert" >
+        <v-list-item-title>Você tem algo pendente</v-list-item-title>
+        <v-list-item-subtitle>Verifique quanto antes para evitar possiveis problemas </v-list-item-subtitle>
+      </v-list-item>
+       <!--  2 -->
+      <v-list-item flat class="border-b" prepend-icon="mdi-bomb">
+        <v-list-item-title>Promoção explosiva</v-list-item-title>
+        <v-list-item-subtitle>Não perca as melhores ofertas</v-list-item-subtitle>
+      </v-list-item>
+
+    </v-list>
+   </v-card>
+        </v-menu>
+
+        <v-menu>
+          <template #activator="{props}">
+            <v-avatar v-bind:="props"  >
           <v-img cover src="https://meragor.com/files/styles//ava_800_800_wm/the_spongebob_movie_sponge_out_of_water_art.jpg"></v-img>
         </v-avatar>
+      </template>   
+   <v-card min-width="200px">
+    <v-list lines="false" density="compact" nav>
+      <!-- 1   -->
+      <v-list-item prepend-icon="mdi-account-outline">
+        <v-list-item-title>Meu perfil</v-list-item-title>
+      </v-list-item>
+       <!--  2 -->
+      <v-list-item prepend-icon="mdi-heart-outline">
+        <v-list-item-title>Favoritos</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+   </v-card>
+      </v-menu>
       </template>
     </v-app-bar>
+
+
+
+
     <v-main>
       <v-container>
         <h1>Dashboard</h1>
@@ -114,18 +181,10 @@
   </v-app>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "App",
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+<script setup>
+import { ref }  from 'vue';
+const isDrawerOpen = ref(false)
 </script>
+
+
